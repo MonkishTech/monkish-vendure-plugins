b3b8caa17bc702621f23122f2a6bf68f22b419dd
barakamwakisha <barakamwakisha@gmail.com>
2025-01-05 13:51:14 +0300

    fix build

--- packages/paystack/src/constants.ts
+++ packages/paystack/src/constants.ts
@@ -1,7 +1,4 @@
 import { CurrencyCode } from './types';
-import { initialData } from '~utils';
-
-const test = initialData;
 
 export const loggerCtx = 'PaystackPlugin';
 export const PLUGIN_INIT_OPTIONS = Symbol('PLUGIN_INIT_OPTIONS');
